8b0d049c8158d3be6616427eedfce4e6d7f7a396
Sean <seanmurphy@Seans-MacBook-Pro.local>
2015-12-10 14:45:07 -0500

    mor options

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$( "#button_1" ).click(function() {
-		var thanks = Array("Gee, Thanks Guys!", "Golly", "No more ramen for TimTim", "Timmy is a free HN");
+		var thanks = Array("Gee, Thanks Guys!", "Golly", "No more ramen for TimTim", "Timmy is a free HN" "This will buy enough Mountian Dew and Doritos to last weeks!", "Any more of this, and I'll be voting Republican!", "Suck it Scoot");
 		var thanks = thanks[Math.floor(Math.random()*thanks.length)];
   		alert( thanks );
 	})
