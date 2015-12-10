c9af10b86a2367b50348e6bc337f8bfd62891307
Sean <seanmurphy@Seans-MacBook-Pro.local>
2015-12-10 14:44:01 -0500

    addtnl options for thanks

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$( "#button_1" ).click(function() {
-  		alert( "Gee, Thanks Guys!" );
+		var thanks = Array("Gee, Thanks Guys!", "Golly", "No more ramen for TimTim", "Timmy is a free HN");
+		var thanks = thanks[Math.floor(Math.random()*thanks.length)];
+  		alert( thanks );
 	})
 });
